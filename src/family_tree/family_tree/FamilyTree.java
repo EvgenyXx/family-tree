d1bd6acb03b17b4815782f66ecf2f43f261ee80e
Evgeny Pavlov <Evgenypavlov666@yandex.ru>
2024-07-05 11:02:05 +0300

    first commit

---
+++ src/family_tree/family_tree/FamilyTree.java
@@ -0,0 +1,35 @@
+package family_tree.family_tree;
+
+import family_tree.human.Human;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class FamilyTree {
+    private final List<Human> persons;
+
+    public FamilyTree() {
+        this.persons = new ArrayList<>();
+    }
+
+    public void addPerson(Human person) {
+        persons.add(person);
+    }
+
+    public void setParents(Human child, Human father, Human mother) {
+        child.setFather(father);
+        child.setMother(mother);
+        father.getChildren().add(child);
+        mother.getChildren().add(child);
+    }
+
+    public List<Human> getChildrenOfPerson(Human person) {
+        List<Human> children = new ArrayList<>();
+        for (Human child : person.getChildren()) {
+            children.add(child);
+        }
+        return children;
+    }
+}
